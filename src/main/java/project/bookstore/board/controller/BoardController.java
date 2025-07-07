45faa6ee321983d86314839105a8ece1c2b082cc
Kim Hyemi <khm24ks@naver.com>
2025-07-07 11:57:25 +0900

    게시판 기능 구현 -> 게시글 등록/수정/삭제, 댓글 등록/수정/삭제 > 추가수정

--- src/main/java/project/bookstore/board/controller/BoardController.java
+++ src/main/java/project/bookstore/board/controller/BoardController.java
@@ -1,11 +1,13 @@
 package project.bookstore.board.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import project.bookstore.board.dto.BoardDetailDto;
 import project.bookstore.board.dto.BoardSaveDto;
@@ -15,7 +17,6 @@ import project.bookstore.board.entity.Board;
 import project.bookstore.board.service.BoardService;
 import project.bookstore.member.entity.Member;
 import project.bookstore.member.security.CustomUserDetails;
-
 
 
 @Controller
@@ -29,26 +30,35 @@ public class BoardController {
 
     @GetMapping("/new")
     public String form(Model model, @AuthenticationPrincipal CustomUserDetails userDetails) {
-        model.addAttribute("boardSaveDto",new BoardSaveDto());
-        model.addAttribute("nickname",userDetails.getMember().getNickname());
+        model.addAttribute("boardSaveDto", new BoardSaveDto());
+        model.addAttribute("nickname", userDetails.getMember().getNickname());
         return "board/boardForm";
     }
 
     // 게시글 등록 처리
     @PostMapping("/new")
-    public String save(@ModelAttribute BoardSaveDto dto,
-                       @AuthenticationPrincipal CustomUserDetails userDetails) {
-        Member writer = userDetails.getMember();
-        boardService.createBoard(dto, writer);
+    public String save(@Valid @ModelAttribute BoardSaveDto dto, BindingResult result,
+                       @AuthenticationPrincipal CustomUserDetails userDetails
+                        , Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("nickname", userDetails.getMember().getNickname());
+            return "board/boardForm";
+        }
+        try {
+            boardService.createBoard(dto, userDetails.getMember());
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("fileError",e.getMessage());
+            model.addAttribute("nickname", userDetails.getMember().getNickname());
+        }
         return "redirect:/board";
     }
 
     //게시글 목록 + 검색/페이징
     @GetMapping
     public String list(@ModelAttribute BoardSearchCondition condition, Pageable pageable, Model model, @AuthenticationPrincipal CustomUserDetails userDetails) {
-        Member currentUser = (userDetails!= null)? userDetails.getMember():null;
+        Member currentUser = (userDetails != null) ? userDetails.getMember() : null;
         Page<Board> boardPage = boardService.searchBoard(condition, pageable);
-        Page<BoardDetailDto> dtoPage = boardPage.map(board -> BoardDetailDto.from(board,currentUser));
+        Page<BoardDetailDto> dtoPage = boardPage.map(board -> BoardDetailDto.from(board, currentUser));
 
         model.addAttribute("boards", dtoPage);
         model.addAttribute("condition", condition);
@@ -59,7 +69,7 @@ public class BoardController {
     // 게시글 상세
     @GetMapping("/{id}")
     public String detail(@PathVariable Long id, Model model, @AuthenticationPrincipal CustomUserDetails userDetails) {
-        Member currentUser = (userDetails!=null)? userDetails.getMember():null;
+        Member currentUser = (userDetails != null) ? userDetails.getMember() : null;
         BoardDetailDto dto = boardService.getBoardDetail(id, currentUser);
         model.addAttribute("board", dto);
         return "board/boardDetail";
@@ -68,17 +78,17 @@ public class BoardController {
     // 게시글 수정 폼
     @GetMapping("/{id}/edit")
     public String editForm(@PathVariable Long id, Model model, @AuthenticationPrincipal CustomUserDetails userDetails) {
-        Member currentUser = (userDetails!=null)? userDetails.getMember():null;
-        BoardDetailDto dto = boardService.getBoardDetail(id,currentUser);
+        Member currentUser = (userDetails != null) ? userDetails.getMember() : null;
+        BoardDetailDto dto = boardService.getBoardDetail(id, currentUser);
         model.addAttribute("boardUpdateDto", dto.toUpdateDto()); // 폼용 DTO 생성 메서드 추가(필요한 값만 넘겨주기 위해서
-        model.addAttribute("nickname",currentUser.getNickname());
+        model.addAttribute("nickname", currentUser.getNickname());
         return "board/boardEditForm";
     }
 
     // 게시글 수정 처리
     @PostMapping("/{id}/edit")
     public String update(@PathVariable Long id, @ModelAttribute BoardUpdateDto dto) {
-        boardService.updateBoard(id, dto );
+        boardService.updateBoard(id, dto);
         return "redirect:/board/{id}";
     }
 
--- src/main/java/project/bookstore/board/dto/BoardSaveDto.java
+++ src/main/java/project/bookstore/board/dto/BoardSaveDto.java
@@ -1,5 +1,7 @@
 package project.bookstore.board.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,7 +10,13 @@ import java.util.List;
 
 @Getter @Setter
 public class BoardSaveDto {
+
+    @NotBlank(message = "제목은 필수입니다.")
+    @Size(max = 50, message = "제목은 50자 이하로 입력해주세요.")
     private String title;
+
+    @NotBlank(message = "내용은 필수입니다.")
+    @Size(max = 1000, message = "내용은 1000자 이하로 입력해주세요.")
     private String content;
     private List<MultipartFile> files;
 }
